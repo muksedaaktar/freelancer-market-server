12be624181fc8aab65b8cc2c8aac73a0220d2dc9
Mukseda Aktar <muksedamukti@gmail.com>
2026-05-18 07:23:05 +0600

    server site is ready

--- index.js
+++ index.js
@@ -28,7 +28,64 @@ async function run() {
         const db = client.db('market_db');
         const jobsCollection = db.collection('jobs');
         const userscCollection = db.collection('users');
+        const acceptedTasksCollection = db.collection('acceptedTasks');
 
+        //Accepted Task APIs
+        app.post('/acceptedTasks', async (req, res) => {
+
+            const acceptedTask = req.body;
+
+            const { _id, ...rest } = acceptedTask;
+
+            // ❌ prevent own job acceptance
+            if (acceptedTask.postedBy === acceptedTask.acceptedBy) {
+                return res.status(403).send({
+                    message: "You cannot accept your own job"
+                });
+            }
+
+            const result = await acceptedTasksCollection.insertOne(rest);
+
+            res.send(result);
+        });
+
+        app.get('/acceptedTasks', async (req, res) => {
+
+            const email = req.query.email;
+            const query = {
+                acceptedBy: email
+            };
+            const result = await acceptedTasksCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.delete('/acceptedTasks/:id', async (req, res) => {
+
+            try {
+
+                const id = req.params.id;
+
+                const query = {
+                    _id: new ObjectId(id)
+                };
+
+                const result = await acceptedTasksCollection.deleteOne(query);
+
+                res.send(result);
+
+            }
+            catch (error) {
+
+                console.log(error);
+
+                res.status(500).send({
+                    error: "Delete failed"
+                });
+
+            }
+
+        });
+        //users APIs
         app.post('/users', async (req, res) => {
             const newUser = req.body;
 
@@ -48,27 +105,37 @@ async function run() {
         })
 
 
-        app.get('/latest-jobs',async(req,res) => {
-            const cursor = jobsCollection.find().sort({createdAt : -1}).limit(6);
+        //get latest job
+        app.get('/latest-jobs', async (req, res) => {
+            const cursor = jobsCollection.find().sort({ createdAt: -1 }).limit(6);
             const result = await cursor.toArray();
             res.send(result);
         })
 
 
+        //get all jobs
         app.get('/jobs', async (req, res) => {
 
-            console.log(req.query)
-            const email = req.query.email
-            const query = {}
+            const email = req.query.email;
+            const category = req.query.category;
+
+            const query = {};
+
             if (email) {
                 query.userEmail = email;
             }
 
-            const cursor = jobsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+            if (category) {
+                query.category = category;
+            }
 
+            const cursor = jobsCollection.find(query).sort({ createdAt: -1 });
+            const result = await cursor.toArray();
+
+            res.send(result);
+        });
+
+        //get one job
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -77,7 +144,7 @@ async function run() {
         })
 
 
-
+        //post job
         app.post('/jobs', async (req, res) => {
 
             const newJob = {
@@ -90,6 +157,7 @@ async function run() {
             res.send(result);
         })
 
+        //update job
         app.patch('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const updatedJob = req.body;
@@ -108,6 +176,7 @@ async function run() {
             res.send(result);
         })
 
+        //delete job
         app.delete('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
